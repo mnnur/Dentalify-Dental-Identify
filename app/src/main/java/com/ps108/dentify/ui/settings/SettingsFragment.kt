30e1a2026592834ef7d0c1d24efbe5393cf9c6a1
AzCasanova <irvanea1@gmail.com>
2023-11-30 23:01:38 +0700

    add printilan

--- app/src/main/java/com/ps108/dentify/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ps108/dentify/ui/settings/SettingsFragment.kt
@@ -5,24 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.ps108.dentify.WelcomeActivity
 import com.ps108.dentify.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
-    private lateinit var binding : FragmentSettingsBinding
+    private lateinit var binding: FragmentSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSettingsBinding.inflate(inflater,container, false)
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
-        binding.btnLogout.setOnClickListener {
+        binding.btnLog.setOnClickListener {
             startActivity(Intent(requireActivity(), WelcomeActivity::class.java))
         }
 
